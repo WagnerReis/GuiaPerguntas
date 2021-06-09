14731b515b880691e3180d1b82514aface257c20
WagnerReis <wagnerreis1997@gmail.com>
2021-06-09 14:43:16 -0300

    Busca condicional com sequelize

--- index.js
+++ index.js
@@ -48,4 +48,17 @@ app.post("/salvarpergunta",(req, res) => {
   })
 });
 
+app.get("/pergunta/:id",(req, res) =>{
+  var id = req.params.id;
+  Pergunta.findOne({
+    where: { id: id}
+  }).then(pergunta => {
+    if(pergunta != undefined) { //Pergunta encontrada
+      res.render("pergunta");
+    }else{//Não encontrada
+      res.redirect("/");
+    }
+  });
+});
+
 app.listen(8080, () => { console.log("App rodando!")});
