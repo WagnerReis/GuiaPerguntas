9201f3f13e1b032d00924215b68a583eacb65114
WagnerReis <wagnerreis1997@gmail.com>
2021-06-09 19:14:14 -0300

    Exibição de respostas e finalização

--- index.js
+++ index.js
@@ -55,9 +55,21 @@ app.get("/pergunta/:id",(req, res) =>{
     where: { id: id}
   }).then(pergunta => {
     if(pergunta != undefined) { //Pergunta encontrada
-      res.render("pergunta", {
-        pergunta: pergunta
-      });
+
+      Resposta.findAll({
+        where: {perguntaId: pergunta.id},
+        order: [
+          ['id', 'DESC']
+        ]
+      }).then(respostas => {
+        res.render("pergunta", {
+          pergunta: pergunta,
+          respostas: respostas
+        });
+      }).catch((erro) => {
+        console.log(erro);
+      });;
+
     }else{//Não encontrada
       res.redirect("/");
     }
