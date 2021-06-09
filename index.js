9cc461d65e57e519e976993bdf2f16b761226075
WagnerReis <wagnerreis1997@gmail.com>
2021-06-09 11:16:28 -0300

    Capturando dados de um formulario

--- index.js
+++ index.js
@@ -1,15 +1,28 @@
 const express = require("express");
 const app = express();
+const bodyParser = require('body-parser');
 
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
+//body parser
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+//rotas
 app.get("/",(req, res) => {
   res.render("index");
 });
 
-app.get("/perguntar",(req,res) => {
+app.get("/perguntar",(req, res) => {
   res.render("perguntar");
+});
+
+app.post("/salvarpergunta",(req, res) => {
+  // console.log = req.body;
+  var titulo = req.body.titulo;
+  var descricao = req.body.descricao;
+  res.send("Formulário recebido! titulo: " + titulo + " " + " descricao " + descricao);
 });
 
 app.listen(8080, () => { console.log("App rodando!")});
